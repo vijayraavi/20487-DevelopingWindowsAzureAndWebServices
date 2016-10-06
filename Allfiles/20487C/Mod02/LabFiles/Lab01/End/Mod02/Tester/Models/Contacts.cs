d9907ceb88815ed95352907abe4742a6e026e77e
Baruch Toledano <barucht@sela.co.il>
2016-10-06 13:41:09 +0300

    Reordering folder structure to support B and C versions

---
+++ Allfiles/20487C/Mod02/LabFiles/Lab01/End/Mod02/Tester/Models/Contacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Models
+{
+    class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
---
+++ Allfiles/20487C/Mod02/LabFiles/Lab01/End/Mod02/src/Mod02/Models/Contacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mod02.Models
+{
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
