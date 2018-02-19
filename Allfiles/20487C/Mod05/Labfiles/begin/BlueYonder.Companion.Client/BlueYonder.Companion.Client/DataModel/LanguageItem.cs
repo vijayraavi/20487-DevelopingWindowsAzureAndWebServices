ddb143e2373149cad2f03b81299c502cd43f0cd9
Nadav Har Tzvi <nadavhartzvi@gmail.com>
2018-02-19 14:32:50 +0200

    missing files in client

---
+++ Allfiles/20487C/Mod05/Labfiles/begin/BlueYonder.Companion.Client/BlueYonder.Companion.Client/DataModel/LanguageItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueYonder.Companion.Client.DataModel
+{
+    public class LanguageItem
+    {
+        public string DisplayName { get; set; }
+        public string LanguageTag { get; set; }
+    }
+}
