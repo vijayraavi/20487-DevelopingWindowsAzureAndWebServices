997b4158fcbf25d03d062c049aebdbbc27d0a3a9
Nadav Har Tzvi <nadavhartzvi@gmail.com>
2018-02-26 18:43:42 +0200

    Mod 12 demo

---
+++ Allfiles/20487C/Mod12/DemoFiles/ScalingAppServices/ScalableAzureApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.Mvc;
+using ScalableAzureApp.Models;
+
+namespace ScalableAzureApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly GlobalSharedContext _sharedContext = new GlobalSharedContext();
+
+        public ActionResult Index(CountingModel model)
+        {
+            model.RoleName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME");
+            string instanceId = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID")?.Substring(0, 6).ToUpper();
+
+            if (string.IsNullOrEmpty(model.RoleName))
+                model.RoleName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+            if (string.IsNullOrEmpty(instanceId))
+                instanceId = Environment.MachineName;
+
+            model.CurrentInstance = instanceId;
+
+            ViewBag.Message = $"[{model.RoleName}] [{instanceId}] Your application description page.";
+
+            if (model.IncrementValue == 0)
+            {
+                model.CounterValue = _sharedContext.GetCounter();
+            }
+            else
+            {
+                model.CounterValue = _sharedContext.UpdateCounter(model.IncrementValue);
+            }
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Allfiles/20487C/Mod12/DemoFiles/ScalingAppServices/ScalableAzureApp/Models/CountingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScalableAzureApp.Models
+{
+    public class CountingModel
+    {
+        public long IncrementValue { get; set; } = 1;
+        public long CounterValue { get; set; } = 0;
+        public string CurrentInstance { get; set; } = "?";
+        public string RoleName { get; set; } = "?";
+        //public IList<string> Instances { get; set; } = new List<string>();
+    }
+}
